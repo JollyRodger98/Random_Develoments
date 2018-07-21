0564f8ba2f476d45211969c03af30aeca830f4d5
JollyRodger98 <twaldesbuehl@gmail.com>
2018-07-21 21:02:50 +0200

    prime_number script finished

---
+++ Python/Math/prime_num.py
@@ -0,0 +1,10 @@
+for num in range(100000):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+#                print(num, 'is not a prime number')
+                break
+        else:
+            print(num, 'is a prime number.')
+
+                
